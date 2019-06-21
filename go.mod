df9a93e58ee6eff9b667b103671f00e18ed77d01
Gustavo Kawamoto <gkawamoto@valemobi.com.br>
2019-06-21 12:12:40 -0300

    Initial import

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mattermost/mattermost-bot-sample-golang
+
+go 1.12
+
+require (
+	github.com/go-ldap/ldap v3.0.3+incompatible // indirect
+	github.com/mattermost/go-i18n v1.11.0 // indirect
+	github.com/mattermost/mattermost-server v5.12.0+incompatible
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190618222545-ea8f1a30c443 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/mattermost/mattermost-server/model"
+)
+
+var client *model.Client4
+var webSocketClient *model.WebSocketClient
+
+func main() {
+	client = model.NewAPIv4Client("http://localhost:8085")
+	client.MockSession("<bot token>")
+
+	// Lets start listening to some channels via the websocket!
+	webSocketClient, err := model.NewWebSocketClient4("ws://localhost:8085", client.AuthToken)
+	if err != nil {
+		println("We failed to connect to the web socket")
+		PrintError(err)
+	}
+
+	webSocketClient.Listen()
+
+	go func() {
+		for {
+			select {
+			case resp := <-webSocketClient.EventChannel:
+				HandleWebSocketResponse(resp)
+			}
+		}
+	}()
+
+	// You can block forever with
+	select {}
+}
+
+func SendMessage(msg string, channelId string) {
+	post := &model.Post{}
+	post.ChannelId = channelId
+	post.Message = msg
+
+	if _, resp := client.CreatePost(post); resp.Error != nil {
+		println("We failed to send a message to the logging channel")
+		PrintError(resp.Error)
+	}
+}
+
+func HandleWebSocketResponse(event *model.WebSocketEvent) {
+	// Lets only reponded to messaged posted events
+	if event.Event != model.WEBSOCKET_EVENT_POSTED {
+		return
+	}
+
+	var post = model.PostFromJson(strings.NewReader(event.Data["post"].(string)))
+	if post != nil {
+		var me, _ = client.GetMe("")
+		if me == nil {
+			print("Error getting me")
+			return
+		}
+		// Ignoring me
+		if post.UserId == me.Id {
+			return
+		}
+		SendMessage("echo: "+post.Message, post.ChannelId)
+	}
+}
+
+func PrintError(err *model.AppError) {
+	println("\tError Details:")
+	println("\t\t" + err.Message)
+	println("\t\t" + err.Id)
+	println("\t\t" + err.DetailedError)
+}
